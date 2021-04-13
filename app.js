f6329941ed56d12cccad8cd435188bba2ef17256
Mahmoud Elkordy <mahmoud_elkordy65@yahoo.com>
2021-04-13 16:22:06 +0200

    hover

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-$(".col").click(function() {
-  setTimeout($(this).addClass('card-rotate'),3000);
+$(".spider").mouseenter(function(){
+  $(this).animate({height: '140px', opacity:0.5});
 })
+
+$(".spider").mouseleave(function(){
+  $(this).animate({height: '130px', opacity:1});
+})
+
+$(".col").click(function() {
+  $(this).addClass('card-rotate');
+})
+
+// $(".col").dblclick(function() {
+//   $(this).removeClass('card-rotate');
+//   $(this).off("click");
+//   $(this).addClass('card-reverse-rotate');
+// })
